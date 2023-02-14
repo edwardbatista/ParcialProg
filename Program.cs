53d535864230c70c267023343b6368697196af85
edwardbatista <64617258+edwardbatista@users.noreply.github.com>
2023-02-13 21:35:15 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ParcialProgramacion;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        static string rot13(string palabra)
+        {
+            char[] letras = palabra.ToCharArray();
+
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                char x = palabra[i];
+                if (x >= 'A' && x <= 'M')
+                {
+                    letras[i] = (char)(x + 13);
+                }
+                else if (x >= 'N' && x <= 'Z')
+                {
+                    letras[i] = (char)(x - 13);
+                }
+                
+            }
+            return new string(letras);
+        }
+
+        Console.WriteLine(rot13("NLRE RFGNON YYBIVRAQB..."));
+        Console.ReadKey();
+    }
+}
+
